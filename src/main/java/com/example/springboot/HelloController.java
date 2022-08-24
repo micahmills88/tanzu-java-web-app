5aaee5f726dacb16a39707ba0c81b35bd1fb6ad7
micah mills <mdmills88@gmail.com>
2022-08-24 14:23:30 -0400

    updated stuff

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu!";
+		return "Don't be fooled by the rocks that i've got!";
 	}
 
 }
